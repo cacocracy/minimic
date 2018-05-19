c6d02df9a2d20f25f25095cf60693ce0917c5b08
Developer <None>
2018-05-19 15:09:56 -0400

    minimic/__init__.py

---
+++ minimic/__init__.py
@@ -0,0 +1,5 @@
+from client import SessionClient
+from mmparsing import save_album, save_profile
+from misc import DEFAULT_USER_AGENT
+from exceptions import MinimicException, LoginError, ParseError, SkippedAlbum
+
