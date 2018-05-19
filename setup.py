9c005f2123dce54c12c7f09211a1c5693504b52f
Developer <None>
2018-05-19 13:23:00 -0400

    Initial packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='minimic',
+      version='0.0',
+      description='Tools for minimic',
+      url=None,
+      author='Cacocracy',
+      author_email=None,
+      license='Unlicense',
+      packages=['minimic'],
+      zip_safe=False,
+      scripts=['bin/minimic'])
+
